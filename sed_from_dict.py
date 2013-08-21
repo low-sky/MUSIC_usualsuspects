c1e43a0dda59ef02b7b10b42947771eed57dcce2
Adam Ginsburg <keflavich@gmail.com>
2013-08-20 20:43:19 -0600

    divide by ppbeam; more inline docs

--- sed_from_dict.py
+++ sed_from_dict.py
@@ -2,6 +2,11 @@ import numpy as np
 import pylab as pl
 from mad import MAD
 
+# PPBEAM?
+# In [62]: data[0].mapstruct.omega_beam_am / data[3].mapstruct.omega_pix_am
+# Out[62]: array([ 46.82668131])
+ppbeam = 46.82668
+
 band_waves = {0: 1.98, 1: 1.3, 2: 1.0, 3: 0.86}
 
 def sed_from_dict(data, TestRadius=5.1, InnerApp=10, OuterApp=13):
@@ -42,11 +47,19 @@ def sed_from_dict(data, TestRadius=5.1, InnerApp=10, OuterApp=13):
     rr = ((yy-ycen)**2 + (xx-xcen)**2)**0.5
 
     # not used counts = np.nansum(np.nansum((rr1 < TestRadius)*(cube>0),axis=1),axis=1)
+
+    # 2D mask
     mask = (rr < InnerApp)
-    fluxes = np.nansum(cube[:,mask],axis=1)
+    # Extract along first (spectral) with [:,mask], which yields something of shape [4, npix]
+    # then, sum along the "spatial" axis to get something of shape [4]
+    # Divide by ppbeam after summing.
+    fluxes = np.nansum(cube[:,mask],axis=1) / ppbeam
+
     outermask = (rr > InnerApp) * (rr < OuterApp)
-    background = np.median(cube[:,outermask],axis=1)
-    errors = MAD(cube[:,outermask],axis=1)
+    background = np.median(cube[:,outermask],axis=1) / ppbeam
+
+    # compute the errors using the median absolute deviation.  These are too small, I think...
+    errors = MAD(cube[:,outermask],axis=1) / ppbeam
 
     return fluxes,background,errors
 
